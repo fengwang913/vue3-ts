52b0dbc41a0b2d5f0b475865a09aa3e498652715
wangfeng <wangfeng@bdrl.com.cn>
2023-08-08 10:46:07 +0800

    feat: add element-plus

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-button type="primary">I am elbutton</el-button>
   </div>
 </template>
+
+
+<script lang="ts">
+import { ElButton } from 'element-plus'
+export default {
+    components: { ElButton },
+  }
+
+
+</script>
 
 <style>
 @media (min-width: 1024px) {
